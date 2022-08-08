5488366584cfc4a2a2794a1dfdce6c18c1fc7053
Danil <31danet@gmail.com>
2022-08-08 21:52:10 +0300

    added task_6

---
+++ task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0) 
+{
+    Console.WriteLine("Введенное Вами число является четным");
+}
+else 
+{
+    Console.Write("Введенное Вами число является нечетным");
+}
