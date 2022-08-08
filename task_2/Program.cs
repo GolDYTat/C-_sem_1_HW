790053bf40512227a1b765a138b8bb4d66f48952
Danil <31danet@gmail.com>
2022-08-08 21:49:32 +0300

    added task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num < num1)
+{
+    Console.WriteLine($"Наибольшее число: {num1}. Наименьшее число: {num}");
+}
+else {
+    Console.WriteLine($"Наибольшее число: {num}. Наименьшее число: {num1}");
+}
