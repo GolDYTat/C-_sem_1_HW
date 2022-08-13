ee2d90e3bea433686a272cb3a54afa34c5eb0d17
Danil <31danet@gmail.com>
2022-08-13 14:35:36 +0300

    added task13

---
+++ task13/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ReturnThird(int num)
+{
+    int result = 0;
+    while (num >= 1000) 
+    {
+        num = num / 10;
+    }
+    if (num < 100) return -1;
+    return result = num % 10;
+}
+
+int res = ReturnThird(number);
+
+if (res != -1) Console.WriteLine($"Третья цифра числа: {res}");
+else Console.WriteLine($"Третьей цифры нет");
