4203adfb7e8b3199d0816cf785559da101d35547
Danil <31danet@gmail.com>
2022-08-13 15:04:59 +0300

    added task15

---
+++ task15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число от 1 до 7, соответствующее дню недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int DayWeek(int num)
+{
+    int result = 0;
+    if (num < 6 && num > 0) return result = result - 1;
+    else if (num > 5 && num < 8) return result = result + 1;
+    return result;
+}
+
+int res = DayWeek(number);
+if (res == 1) Console.Write(" -> Да");
+else if (res == -1) Console.Write(" -> Нет");
+else Console.Write("Введите корректное число");
