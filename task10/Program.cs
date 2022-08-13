139f2c4a080c85e4e07dfde85d84837d7c7ec1e8
Danil <31danet@gmail.com>
2022-08-13 13:18:36 +0300

    added task10

---
+++ task10/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ReturnSecond(int num)
+{
+    int num1 = num % 100;
+    int num2 = num1 / 10;
+    return num2;
+}
+
+if (number > 99 && number < 1000) 
+{
+    int res = ReturnSecond(number);
+    Console.WriteLine($"Вторая цифра числа {number} ==> {res}");
+}
+else Console.WriteLine("Некорректный ввод, пожалуйста, введите трехзначное число");
