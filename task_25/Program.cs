403743eeb9a31c10b43eae244fa5657ba2c87c52
Danil <31danet@gmail.com>
2022-08-16 19:26:32 +0300

    added task_25

---
+++ task_25/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Введите два числа: ");
+Console.Write("Первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int Multipl(int a, int b)
+{
+    int res = 1;
+    for (int i = 0; i < b; i++)
+    {
+        res = res * a;
+    }
+    return res;
+}
+
+int result = Multipl(x, y);
+Console.WriteLine($"{x} в степени {y} = {result}");
