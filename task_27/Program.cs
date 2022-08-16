6d3e8ca743d0e02f47ace0b8579340474025bd15
Danil <31danet@gmail.com>
2022-08-16 20:19:31 +0300

    added task_29

--- task_27/Program.cs
+++ task_27/Program.cs
@@ -14,7 +14,6 @@ int Count(int numb)
 }
 
 int count = Count(number);
-Console.WriteLine($"{count}");
 
 int SumDigit(int num)
 {
---
+++ task_29/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+Console.WriteLine("Введите размер массива: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 0) Console.WriteLine("Введите положительное число");
+
+int[] Array(int size)
+{
+    int[] arr = new int[size];
+    Random random = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = random.Next(1, 100);
+    }
+    return arr;
+}
+
+int[] result = Array(number - 1);
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]},");
+    }
+    Console.Write($"{arr[arr.Length - 1]}]");
+}
+PrintArray(result);
