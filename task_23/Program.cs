b8156a21f8b4459d9620e913ae205d1b6888130d
Danil <31danet@gmail.com>
2022-08-16 16:15:14 +0300

    added task_23

---
+++ task_23/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1) Console.WriteLine("Введите положительное число");
+
+void TableCube (int num)
+{
+    for (int i = 1; i <= num; i++)
+    Console.WriteLine($"{i}  {i*i*i}");
+}
+
+TableCube(number);
