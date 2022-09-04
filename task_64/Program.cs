10513580f3c7c7162a378719a2513e94fb9f8fdd
Danil <31danet@gmail.com>
2022-09-05 01:06:00 +0300

    added task_64

---
+++ task_64/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumber(int num)
+{
+    if (num < 1) return;
+    Console.Write($"{num} ");
+    NaturalNumber(num - 1);    
+}
+
+NaturalNumber(number);
