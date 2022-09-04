cd2bd4f8b77c572d4a3346d50675f8df3cc48207
Danil <31danet@gmail.com>
2022-09-05 02:07:47 +0300

    added task_68

---
+++ task_68/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите неотрицательное число: ");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите неотрицательное число: ");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+
+int sum = AkkermanFunc(numberFirst, numberSecond);
+Console.WriteLine($"Функция Аккермана: А({numberFirst},{numberSecond}) = {sum} ");
+
+int AkkermanFunc(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (n == 0) return AkkermanFunc(m - 1, 1);
+    return AkkermanFunc(m-1, AkkermanFunc(m, n - 1));
+}
